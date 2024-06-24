deab93d9717dc6973b42b6bbdcc6659a7d4dafe5
Lucasgmendonca <lucasgarciam.aut@gmail.com>
2024-06-24 20:08:49 -0300

    Adição da função numecore na aplicação

--- src/TCC/speckle.py
+++ src/TCC/speckle.py
@@ -245,31 +245,224 @@ class PlotGraph:
         plt.legend()
         plt.show()
 
-class SaveResults:
-    """Classe responsável por salvar os resultados em arquivos de texto."""
 
-    def __init__(self, pixel_history_data):
-        """Inicializa a classe SaveResults.
+# class SavePixelHistory:
+#     """Classe responsável por salvar o histórico de pixels em um arquivo de texto."""
+
+#     def __init__(self, pixel_history_data):
+#         """Inicializa a classe SavePixelHistory.
+
+#         Args:
+#             pixel_history_data: Dados do histórico de pixels.
+#         """
+#         self.pixel_history_data = pixel_history_data
+
+#     def save(self):
+#         """Salva os dados do histórico de pixels em arquivos de texto."""
+#         # Salvando o resultado em um arquivo de texto
+#         with open('output.txt', 'w') as f:
+#             print(self.pixel_history_data, file=f)
+#         print("Resultados salvos em 'output.txt'.")
+
+#         # Salvando o resultado em um arquivo de texto
+#         with open('pixel_history.txt', 'w') as file:
+#             for array in self.pixel_history_data[0]:  # A matriz está na primeira posição da lista
+#                 for value in array:
+#                     file.write(f"{value} ")
+#                 file.write("\n")
+#         print("Histórico dos pixels salvo em pixel_history.txt")
+
+class SavePixelHistory:
+    """Classe responsável por salvar o histórico de pixels e o gráfico do histórico."""
+
+    def __init__(self, pixel_history_data, first_image_number, last_image_number, pixel_index):
+        """
+        Inicializa a classe SavePixelHistory.
 
         Args:
             pixel_history_data: Dados do histórico de pixels.
+            first_image_number: Número da primeira imagem.
+            last_image_number: Número da última imagem.
+            pixel_index: Índice do pixel a ser plotado.
         """
         self.pixel_history_data = pixel_history_data
+        self.first_image_number = first_image_number
+        self.last_image_number = last_image_number
+        self.pixel_index = pixel_index
 
     def save(self):
-        """Salva os dados do histórico de pixels em arquivos de texto."""
+        """Salva os dados do histórico de pixels em arquivos de texto e o gráfico em um arquivo de imagem."""
         # Salvando o resultado em um arquivo de texto
         with open('output.txt', 'w') as f:
             print(self.pixel_history_data, file=f)
         print("Resultados salvos em 'output.txt'.")
 
-        # Salvando o resultado em um arquivo de texto
+        # Salvando o histórico dos pixels em um arquivo de texto
         with open('pixel_history.txt', 'w') as file:
             for array in self.pixel_history_data[0]:  # A matriz está na primeira posição da lista
                 for value in array:
                     file.write(f"{value} ")
                 file.write("\n")
-        print("Histórico dos pixels salvo em pixel_history.txt")
+        print("Histórico dos pixels salvo em 'pixel_history.txt'")
+
+        # Salvando o gráfico do histórico de intensidade do pixel escolhido
+        plt.figure()
+        plt.plot(range(self.first_image_number, self.last_image_number + 1), self.pixel_history_data[0][self.pixel_index], label=f'Pixel {self.pixel_index + 1}')
+        plt.xlabel('Número da Imagem')
+        plt.ylabel('Intensidade')
+        plt.title('Histórico de Intensidade de Pixel')
+        plt.legend()
+        plt.savefig('pixel_history_plot.png')
+        plt.close()
+        print("Gráfico do histórico dos pixels salvo em 'pixel_history_plot.png'")
+
+        
+
+
+class SaveAnalysisResults:
+    """Classe responsável por salvar os resultados da análise numérica e a matriz de co-ocorrência."""
+
+    def __init__(self, numerical_result, co_occurrence_matrix):
+        """Inicializa a classe SaveAnalysisResults.
+
+        Args:
+            numerical_result: Resultado numérico da análise.
+            co_occurrence_matrix: Matriz de co-ocorrência.
+        """
+        self.numerical_result = numerical_result
+        self.co_occurrence_matrix = co_occurrence_matrix
+
+    def save(self):
+        # Salvando o resultado numérico em um arquivo de texto
+        with open('numerical_result.txt', 'w') as file:
+            file.write(f"Numerical Result: {self.numerical_result}\n")
+        print("Resultado numérico salvo em 'numerical_result.txt'.")
+
+        # Salvando a matriz de co-ocorrência em um arquivo de texto
+        with open('co_occurrence_matrix.txt', 'w') as file:
+            for row in self.co_occurrence_matrix:
+                for value in row:
+                    file.write(f"{value} ")
+                file.write("\n")
+        print("Matriz de co-ocorrência salva em 'co_occurrence_matrix.txt'.")
+
+
+# class SaveResults:
+#     """Classe responsável por salvar os resultados em arquivos de texto."""
+
+#     def __init__(self, pixel_history_data, numerical_result, co_occurrence_matrix):
+#         """Inicializa a classe SaveResults.
+
+#         Args:
+#             pixel_history_data: Dados do histórico de pixels.
+#             numerical_result: Resultado numérico da análise.
+#             co_occurrence_matrix: Matriz de co-ocorrência.
+#         """
+#         self.pixel_history_data = pixel_history_data
+#         self.numerical_result = numerical_result
+#         self.co_occurrence_matrix = co_occurrence_matrix
+
+#     def save(self):
+#         """Salva os dados do histórico de pixels em arquivos de texto."""
+#         # Salvando o resultado em um arquivo de texto
+#         with open('output.txt', 'w') as f:
+#             print(self.pixel_history_data, file=f)
+#         print("Resultados salvos em 'output.txt'.")
+
+#         # Salvando o resultado em um arquivo de texto
+#         with open('pixel_history.txt', 'w') as file:
+#             for array in self.pixel_history_data[0]:  # A matriz está na primeira posição da lista
+#                 for value in array:
+#                     file.write(f"{value} ")
+#                 file.write("\n")
+#         print("Histórico dos pixels salvo em pixel_history.txt")
+
+#         # Salvando o resultado numérico em um arquivo de texto
+#         with open('numerical_result.txt', 'w') as file:
+#             file.write(f"Numerical Result: {self.numerical_result}\n")
+#         print("Resultado numérico salvo em 'numerical_result.txt'.")
+
+#         # Salvando a matriz de co-ocorrência em um arquivo de texto
+#         with open('co_occurrence_matrix.txt', 'w') as file:
+#             for row in self.co_occurrence_matrix:
+#                 for value in row:
+#                     file.write(f"{value} ")
+#                 file.write("\n")
+#         print("Matriz de co-ocorrência salva em 'co_occurrence_matrix.txt'.")
+
+
+class NumericalAnalysis:
+    """Classe responsável por realizar análises numéricas no histórico de pixels."""
+
+    def __init__(self, pixel_history_data, method_selection):
+        """
+        Inicializa a classe NumericalAnalysis.
+
+        Args:
+            pixel_history_data: Dados do histórico de pixels.
+            method_selection: Método de análise numérica a ser utilizado.
+        """
+        self.pixel_history_data = pixel_history_data
+        self.method_selection = method_selection
+
+    def numecore(self):
+            """Realiza a análise numérica no histórico de pixels."""
+            # Valores de saída padrão
+            num_res = 0
+            co_matr = np.zeros((256, 256))
+
+            # Verificação de parâmetros
+            if (self.method_selection > 5):
+                raise ValueError('numecore: unknown method of analysis!')
+            
+            # Parâmetros internos
+            sts_lin = self.pixel_history_data[0].shape[0]  # Número de sinais (pixels rastreados)
+            sts_row = self.pixel_history_data[0].shape[1]  # Comprimento dos sinais (número de imagens)
+
+            # Geração da matriz de co-ocorrência (se necessário)
+            if self.method_selection < 3 or self.method_selection > 4:
+                co_matr = np.zeros((256, 256))
+                for idx_lin in range(sts_lin):
+                    for idx_row in range(sts_row - 1):
+                        pix_now = round(self.pixel_history_data[0][idx_lin, idx_row])  # intensidade do pixel analisado
+                        pix_nxt = round(self.pixel_history_data[0][idx_lin, idx_row + 1])  # intensidade do próximo pixel
+                        co_matr[pix_now, pix_nxt] += 1  # contador de ocorrências
+
+            # Momento de inércia
+            if self.method_selection == 1:
+                for idx_lin in range(256):
+                    for idx_row in range(256):
+                        num_res += co_matr[idx_lin, idx_row] * (idx_lin - idx_row) ** 2
+                nrmfac = (sts_row - 1) * sts_lin  # maior quantidade de mudanças que pode ocorrer no STS analisado
+                num_res /= nrmfac
+
+            # Valor absoluto das diferenças
+            if self.method_selection == 2:
+                for idx_lin in range(256):
+                    for idx_row in range(256):
+                        num_res += co_matr[idx_lin, idx_row] * abs(idx_lin - idx_row)
+                nrmfac = (sts_row - 1) * sts_lin  # maior quantidade de mudanças que pode ocorrer no STS analisado
+                num_res /= nrmfac
+
+            # Desvio padrão
+            if self.method_selection == 3:
+                num_res = np.mean(np.std(self.pixel_history_data[0], axis=1))
+
+            # Intensidade média do pixel
+            if self.method_selection == 4:
+                num_res = np.mean(self.pixel_history_data[0])
+
+            # Novo valor absoluto das diferenças
+            if self.method_selection == 5:
+                for idx_lin in range(256):
+                    for idx_row in range(256):
+                        if idx_lin + idx_row != 0:  # Adicione esta verificação
+                            num_res += co_matr[idx_lin, idx_row] * (abs(idx_lin - idx_row) / (idx_lin + idx_row))
+                nrmfac = (sts_row - 1) * sts_lin  # maior quantidade de mudanças que pode ocorrer no STS analisado
+                num_res /= nrmfac
+
+            return num_res, co_matr
+
 
 class Main:
     """Classe principal para executar o programa."""
@@ -289,9 +482,13 @@ class Main:
         pixel_selection = 'a'  # Modo de seleção de pixels: 'a', 'h', 'v', 'r'
         selection_specific = None  # Pode ser 'm', 'e' ou um número específico dependendo do modo
         pixel_index = 0  # Índice do pixel escolhido
+        method_selection = 3  # Método de análise: '1', '2', '3', '4', '5'
 
         preview = Preview(region_of_interest)
         preview.open_preview()
+
+
+        ### captcore ###
 
         capture = Capture(preview.video_input_object, output_path, frame_grab_interval, region_of_interest, file_name, first_image_number, last_image_number)
         capture.capture_images()
@@ -299,16 +496,26 @@ class Main:
         # Libera a captura de vídeo
         preview.video_input_object.release()
 
+
+        ### thspcore ###
+
         pix_hist = PixelHistory(output_path, file_name, first_image_number, last_image_number, pixel_selection, selection_specific)
         pixel_history_data = pix_hist.track_pixel_history()
 
-        # Salvando o resultado em um arquivo de texto
-        save_results = SaveResults(pixel_history_data)
-        save_results.save()
-        
-        # Plotando o gráfico do histórico de intensidade do pixel escolhido
-        plot_graph = PlotGraph(pixel_history_data, first_image_number, last_image_number, pixel_index)
-        plot_graph.plot()
+        # Salvando o histórico de pixels e o gráfico em arquivos
+        save_pixel_history = SavePixelHistory(pixel_history_data, first_image_number, last_image_number, pixel_index)
+        save_pixel_history.save()
+
+
+        ### numecore ###
+
+        # Análise numérica do histórico de pixels
+        num_analysis = NumericalAnalysis(pixel_history_data, method_selection)
+        num_res, co_matr = num_analysis.numecore()
+
+        # Salvando os resultados da análise numérica e a matriz de co-ocorrência
+        save_analysis_results = SaveAnalysisResults(num_res, co_matr)
+        save_analysis_results.save()
 
 if __name__ == "__main__":
     Main.run()
